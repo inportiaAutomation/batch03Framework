7d1a3b9b55717d1d425cb65dacb99df3a32b1fb5
abhijittarale <atarale29@gmail.com>
2016-08-20 10:54:31 +0530

    second commit with app runner and cucumber options

---
+++ batch03Framework/Feature/Login/Login.feature
@@ -0,0 +1,6 @@
+Feature: Sample Login
+
+Scenario: Sample login 
+* click on the login button
+* enter username and password
+* click submit
--- batch03Framework/src/main/java/com/inportia/batch03Framework/App.java
+++ batch03Framework/src/main/java/com/inportia/batch03Framework/App.java
@@ -1,13 +1,18 @@
 package com.inportia.batch03Framework;
 
-/**
- * Hello world!
- *
- */
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions 
+(
+		features = "Feature",
+		glue = "com/nportia/batch03Framework/stepDefs"
+)
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+    
 }
